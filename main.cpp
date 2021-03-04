3984eec95e783d9d2e575269f6b70a189c7092db
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-04 09:35:43 -0600

    Distancia entre dos puntos con Objetos

--- main.cpp
+++ main.cpp
@@ -1,21 +1,95 @@
 #include <iostream>
+#include <fstream>
 #include <math.h>
 using namespace std;
 
+class Punto{
+private:
+    double x;
+    double y;
+public:
+    Punto(void){
+        this->x = 0.0;
+        this->y = 0.0;
+        //cout << "Objeto construido, this -> " << this << endl;
+    };
+    Punto(double x, double y){
+        this->x = x;
+        this->y = y;
+        //cout << "Objeto construido, this -> " << this << endl;
+    };
+    ~Punto(void){
+        //cout << "Objeto destruido, this -> " << this << endl;
+    };
+    void pideleAlUsuarioTusDatos(void){
+        cout<<"Dame mi x ";
+        cin>>this->x;
+        cout<<"Dame mi y ";
+        cin>>this->y;
+    };
+    void muestraTusDatos(void){
+        cout << "(" << this->x << ", " << this->y <<")";
+    };
+    void guardaTusDatos(ofstream& ASalida) {
+        ASalida << "(" << this->x << ", " << this->y << ")" << endl;
+    };
+    void cargaTusDatos(ifstream& AEntrada) {
+        char caracter;
+        AEntrada >> caracter;//Se extrae del archivo un parentesis
+        AEntrada >> this->x;//Se extrae del archivo mi coordenada x
+        AEntrada >> caracter;//Se extrae del archivo una coma
+        AEntrada >> this->y;//Se extrae del archivo mi coordenada y
+        AEntrada >> caracter;//Se extrae del archivo un parentesis
+    };
+    double dameTuX(void){
+        return this->x;
+    };
+    void modificaTuX(double x){
+        this->x = x;
+    };
+    double dameTuY(void){
+        return this->y;
+    };
+    void modificaTuY(double y){
+        this->y = y;
+    };
+    void modificaTusDatos(double x, double y){
+        this->x = x;
+        this->y = y;
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main(void){
-//    Codifica un programa en C++ (cin y cout) que calcule la pendiente
-//    de una recta que pasa a traves de dos puntos en un plano cartesiano
+    Punto P1, P2;
+    double d;
 
-    double x1,y1,x2,y2,m;
+    P1.pideleAlUsuarioTusDatos();
+    P2.pideleAlUsuarioTusDatos();
 
-    cout<<"Dame x1 ";cin>>x1;
-    cout<<"Dame y1 ";cin>>y1;
-    cout<<"Dame x2 ";cin>>x2;
-    cout<<"Dame y2 ";cin>>y2;
+    d = sqrt(
+            pow(P2.dameTuX()-P1.dameTuX(),2)
+             +
+            pow(P2.dameTuY()-P1.dameTuY(),2)
+        );
 
-    m = (y2-y1) / (x2-x1);
-
-    cout<<"m = "<<m<<endl;
+    cout << "d = " << d << endl;
 
     return 0;
 }

f9ce0761253c1c496d204d09ccb8c14948373d19
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-04 09:11:25 -0600

    Pendiente (Fundamentos)

--- main.cpp
+++ main.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 int main(void){
-//    Codifica un programa en C++ (cin y cout) que calcule la distancia
-//    entre dos puntos en un plano cartesiano
+//    Codifica un programa en C++ (cin y cout) que calcule la pendiente
+//    de una recta que pasa a traves de dos puntos en un plano cartesiano
 
-    double x1,y1,x2,y2,d;
+    double x1,y1,x2,y2,m;
 
     cout<<"Dame x1 ";cin>>x1;
     cout<<"Dame y1 ";cin>>y1;
     cout<<"Dame x2 ";cin>>x2;
     cout<<"Dame y2 ";cin>>y2;
 
-    d = sqrt( pow(x2-x1, 2) + pow(y2-y1, 2));
+    m = (y2-y1) / (x2-x1);
 
-    cout<<"d = "<<d<<endl;
+    cout<<"m = "<<m<<endl;
 
     return 0;
 }

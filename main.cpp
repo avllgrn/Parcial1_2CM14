ca5105c4ff3f84cc58b48a836a1f214765885b41
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-12 09:12:40 -0600

    Ejemplo vector en archivo

--- main.cpp
+++ main.cpp
@@ -7,11 +7,47 @@ using namespace std;
 int main(void){
     srand(time(NULL));
 
-    cout<<"Numero entre [0, 10) " << (rand()%10) << endl;
+    int V[100], V2[100], i, n;
 
-    cout<<"Numero entre [0, 50) " << (rand()%50) << endl;
+    cout<<"Ingresa n ";cin>>n;
 
-    cout<<"Numero entre [0, 700) " << (rand()%700) << endl;
+    for(i=0;i<n;i++)
+        cout<<"V2["<<i<<"] = "<<V[i]<<endl;
+    cout<<endl<<endl;
+
+    for(i=0;i<n;i++)
+        V[i] = (rand()%100);
+
+    ofstream ArchivoDeSalida("prueba.txt");
+
+    for(i=0;i<n;i++)
+        ArchivoDeSalida<<V[i]<<endl;
+
+    ArchivoDeSalida.close();
+
+
+
+    ifstream ArchivoDeEntrada("prueba.txt");
+    for(i=0;i<n;i++)
+        ArchivoDeEntrada>>V2[i];
+
+    ArchivoDeEntrada.close();
+
+    for(i=0;i<n;i++)
+        cout<<"V2["<<i<<"] = "<<V[i]<<endl;
+    cout<<endl<<endl;
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+

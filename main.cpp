c501de17bd95bd96b9386ccbd5e02ba566f26bf7
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-18 09:37:09 -0600

    Concatena 2 string en un tercero

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(void){
 
-    string cadena1, cadena2;
+    string cadena1, cadena2, cadena3;
 
     cout<<"cadena1: "<<cadena1<<endl;
     cout<<"cadena2: "<<cadena2<<endl<<endl;
@@ -12,46 +12,15 @@ int main(void){
     cout<<"Ingresa una cadena ";
     getline(cin, cadena1);
 
+    cout<<"Ingresa otra cadena ";
+    getline(cin, cadena2);
+
+    cadena3.append(cadena1);
+    cadena3.append(cadena2);
+
     cout<<"cadena1: "<<cadena1<<endl;
-    cout<<"cadena2: "<<cadena2<<endl<<endl;
-    system("pause");
-
-
-    cadena2.append(cadena1);
-    cout<<"cadena1: "<<cadena1<<endl;
-    cout<<"cadena2: "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena1);
-    cout<<"cadena1: "<<cadena1<<endl;
-    cout<<"cadena2: "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena1);
-    cout<<"cadena1: "<<cadena1<<endl;
-    cout<<"cadena2: "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena1);
-    cout<<"cadena1: "<<cadena1<<endl;
-    cout<<"cadena2: "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena1);
-    cout<<"cadena1: "<<cadena1<<endl;
-    cout<<"cadena2: "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena1);
-    cout<<"cadena1: "<<cadena1<<endl;
-    cout<<"cadena2: "<<cadena2<<endl<<endl;
-    system("pause");
-
-    cadena2.append(cadena1);
-    cout<<"cadena1: "<<cadena1<<endl;
-    cout<<"cadena2: "<<cadena2<<endl<<endl;
-    system("pause");
-
+    cout<<"cadena2: "<<cadena2<<endl;
+    cout<<"cadena3: "<<cadena3<<endl<<endl;
 
     return 0;
 }

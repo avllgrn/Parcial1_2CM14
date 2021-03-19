5b4ca0b40ab71ca2fec98884ee9d917031d7aaa4
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-19 08:57:55 -0600

    find() y rfind() de string

--- main.cpp
+++ main.cpp
@@ -3,24 +3,25 @@ using namespace std;
 
 int main(void){
 
-    string cadena1, cadena2;
+    string cadena1, busqueda;
     int i, n;
 
     cout<<"Ingresa una cadena ";
     getline(cin, cadena1);
+    cout<<"Ingresa  cadena a buscar ";
+    getline(cin, busqueda);
+
+
     n = cadena1.size();
-
-    cout<<"Tu cadena: "<<cadena1<<endl;
-
     for(i=0;i<n;i++){
-        if(!(cadena1.at(i)==' '))
-            cadena2.push_back(cadena1.at(i));
+        cout<<"["<<i<<"] = "<<cadena1.at(i)<<endl;
     }
-    cout<<"cadena1: "<<cadena1<<endl;
-    cout<<"cadena2: "<<cadena2<<endl;
 
-    cout<<cadena1.compare(cadena2)<<endl;
-    cout<<cadena2.compare(cadena1)<<endl;
+    cout<<cadena1.find(busqueda)<<endl;
+    cout<<cadena1.find(' ')<<endl;
+
+    cout<<cadena1.rfind(busqueda)<<endl;
+    cout<<cadena1.rfind(' ')<<endl;
 
     return 0;
 }
